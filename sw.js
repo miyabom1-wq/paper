a6a7d59ce6068df94d2a9ffb56e55b3b4fd91b94
miyabom1-wq <miyabom1-wq@users.noreply.github.com>
2026-07-21 20:09:46 +0900

    Fix PAPER runtime JavaScript

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='paper-standalone-v1';
+const CACHE='paper-standalone-v1-3';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{
   self.skipWaiting();
