bad3ba2f6f976df24cffe02574f1eec40a8ba657
miyabom1-wq <225786137+miyabom1-wq@users.noreply.github.com>
2026-08-31 19:08:32 +0900

    Finalize POSITION CALCULATOR and remove stock quote lookup

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='position-calculator-v3-20260831';
+const CACHE='position-calculator-final-v4-20260831';
 const CORE=['./','./index.html','./manifest.webmanifest','./position-icon-192-v3.png','./position-icon-512-v3.png'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)))});
 self.addEventListener('activate',event=>{event.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]))});
